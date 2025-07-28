e2c39756a97ce6f18e8819d3b659b41b1fa0e9b1
aksoyfiliz <aksoyfiliz09@gmail.com>
2025-07-28 16:58:44 +0300

    new changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,12 +26,11 @@ app.use(cors({
 app.use("/api/auth", authRoute)
 app.use("/api/messages", messageRoute)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.all("/{*any}", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 server.listen(PORT, ()=> {
